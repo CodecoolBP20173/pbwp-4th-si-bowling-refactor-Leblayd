b26f1ffa0e87162a18ef28219bba61b1392e5cb9
Leblayd <leblaydavid7@gmail.com>
2017-12-01 18:52:41 +0100

    started using constants

--- bowling.py
+++ bowling.py
@@ -5,22 +5,22 @@ GUTTER = '-'
 
 def score(game, result=0, frame=1, in_first_half=True):
     for i in range(len(game)):
-        if game[i] == '/':
+        if game[i] == SPARE:
             result += 10 - get_value(game[i - 1])
         else:
             result += get_value(game[i])
 
         if frame < 10 and get_value(game[i]) == 10:    # if character is x or /, and not last round
-            if game[i] == '/':
+            if game[i] == SPARE:
                 result += get_value(game[i + 1])
-            elif game[i].lower() == 'x':
+            elif game[i].lower() == STRIKE:
                 result += get_value(game[i + 1])
-                if game[i + 2] == '/':
+                if game[i + 2] == SPARE:
                     result += 10 - get_value(game[i + 1])
                 else:
                     result += get_value(game[i + 2])
 
-        if game[i].lower() == 'x':
+        if game[i].lower() == STRIKE:
             in_first_half = True
             frame += 1
         elif in_first_half:
