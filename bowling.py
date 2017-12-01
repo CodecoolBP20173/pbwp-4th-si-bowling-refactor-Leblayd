a8d68759ed2d70f03f419b7a6b076b7a76e54f7d
Leblayd <leblaydavid7@gmail.com>
2017-12-01 17:21:23 +0100

    remade get_value

--- bowling.py
+++ bowling.py
@@ -9,19 +9,19 @@ def score(game):
             result += get_value(game[i])
         # if not in_first_half:
             # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                result += get_value(game[i + 1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                result += get_value(game[i + 1])
+                if game[i + 2] == '/':
+                    result += 10 - get_value(game[i + 1])
                 else:
-                    result += get_value(game[i+2])
+                    result += get_value(game[i + 2])
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
@@ -30,6 +30,8 @@ def score(game):
             frame += 1
     return result
 
+
+'''
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
        char == '4' or char == '5' or char == '6' or \
@@ -43,3 +45,15 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
+'''
+
+
+def get_value(char):
+    if char.lower() == 'x' or char == '/':
+        return 10
+    elif char == '-':
+        return 0
+    elif char in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+        return int(char)
+    else:
+        raise ValueError("{} is not a valid score in bowling.".format(char.upper()))
