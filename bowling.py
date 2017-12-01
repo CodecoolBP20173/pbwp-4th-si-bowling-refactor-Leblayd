452f81d7a51bb763d2d15b0b74593746713eeeb1
Leblayd <leblaydavid7@gmail.com>
2017-12-01 19:32:53 +0100

    simplified lowercase converting

--- bowling.py
+++ bowling.py
@@ -20,7 +20,7 @@ def score(game, result=0, frame=1, in_first_half=True):
         the result of the game, integer
     '''
     for i in range(len(game)):
-        current = game[i]
+        current = game[i].lower()
         next_value = get_value(game[(i + 1) % len(game)])
         prev_value = get_value(game[i - 1])
 
@@ -29,17 +29,17 @@ def score(game, result=0, frame=1, in_first_half=True):
         else:
             result += get_value(current)
 
-        if frame < 10 and get_value(current) == 10:    # if character is x or /, and not last round
+        if frame < 10 and get_value(current) == 10:    # if strike or spare, and not 10th round
             if current == SPARE:
                 result += next_value
-            elif current.lower() == STRIKE:
+            elif current == STRIKE:
                 result += next_value
                 if game[i + 2] == SPARE:
                     result += 10 - next_value
                 else:
                     result += get_value(game[i + 2])
 
-        if current.lower() == STRIKE:
+        if current == STRIKE:
             in_first_half = True
             frame += 1
         elif in_first_half:
