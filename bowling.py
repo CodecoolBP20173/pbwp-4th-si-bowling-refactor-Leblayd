0096716b301219b78ee4aa71d30cad407ca0a0c0
Leblayd <leblaydavid7@gmail.com>
2017-12-01 19:29:46 +0100

    Added documentation

--- bowling.py
+++ bowling.py
@@ -4,6 +4,21 @@ GUTTER = '-'
 
 
 def score(game, result=0, frame=1, in_first_half=True):
+    '''
+    A scoring algorhythm for a game of bowling.
+
+    Args:
+        game: a string of characters with numbers, and
+              symbols for spare (/), strike (x), and gutter(-)
+
+    Optional args:
+        result: the base score to start with (integer)
+        frame: the base frame to start with (integer)
+        in_first_half: the base roll to start with (boolean)
+
+    Returns:
+        the result of the game, integer
+    '''
     for i in range(len(game)):
         current = game[i]
         next_value = get_value(game[(i + 1) % len(game)])
@@ -37,6 +52,15 @@ def score(game, result=0, frame=1, in_first_half=True):
 
 
 def get_value(char):
+    '''
+    Converts the symbols in bowling to appropriate values from 1-10.
+
+    Args:
+        single character string to convert
+
+    Returns:
+        integer from 0-10
+    '''
     if char.lower() == STRIKE or char == SPARE:
         return 10
     elif char == GUTTER:
